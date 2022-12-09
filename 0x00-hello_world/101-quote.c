c00117e23e6e471696fb48bc467766405390ac20
Anjames123 <ajibadejames19@gmail.com>
2022-12-09 01:34:35 -0800

    umix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
+
 
 /**
- * main - Entry point "and that piece of art is useful" - Dora korpar, 2015-10-15",
- * followed by a newline, to standard error.
- * Return: Always 1 
+ * main - prints exactly and that piece of art is useful
+ *
+ * Return: Always 0 (Success)
  */
-
 
 int main(void)
 
@@ -13,4 +13,3 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
