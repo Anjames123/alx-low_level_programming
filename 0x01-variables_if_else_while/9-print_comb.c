616e8cb8f9681c8338ee9c0ca08bfb33ed2879fc
Anjames123 <ajibadejames19@gmail.com>
2022-12-09 18:38:25 -0800

    combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,10 @@ int main(void)
 	int a;
 
 	for (a = 0; a < 10; a++)
-	{
+	{	
 		putchar(a + '0');
 		if (a < 9)
-		{
+		{	
 			putchar(',');
 			putchar(' ');
 		}
@@ -21,3 +21,4 @@ int main(void)
 	putchar('\n');
 	return (0);
 	}
+
