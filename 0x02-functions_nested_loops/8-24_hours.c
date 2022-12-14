831c57a5d2e778dcf52c4a95edaf5d9ecf2d19b4
Anjames123 <ajibadejames19@gmail.com>
2022-12-13 17:27:43 -0800

    I'm federal agent Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -24,7 +24,15 @@ void jack_bauer(void)
 			{
 				_putchar(':');
 				_putchar('0');
+				_putchar(j + '0');
 			}
+			else if (j >= 10)
+			{
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			_putchar('\n');
 		}
 	}
 }
